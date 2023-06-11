0f4d9d4e4405c12994a3656863553c1d3a824654
utkarshp11 <utk9621@gmail.com>
2023-06-11 10:52:25 +0530

    we have modified startar class

--- cource-api/src/main/java/io/javabrains/springbootquickstart/courceapi/CourceApiApplication.java
+++ cource-api/src/main/java/io/javabrains/springbootquickstart/courceapi/CourceApiApplication.java
@@ -3,6 +3,7 @@ package io.javabrains.springbootquickstart.courceapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//startar class
 @SpringBootApplication
 public class CourceApiApplication {
 
